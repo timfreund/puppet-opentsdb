4ddc7e609acfb14e4376ae2a10960a14403f9b3e
Tim Freund <tim@freunds.net>
2014-11-15 07:42:31 -0500

    Change -o to -O so we get the package, not log output

--- files/opentsdb-install.sh
+++ files/opentsdb-install.sh
@@ -5,7 +5,7 @@ OTSDB_FILE_DESTINATION=/tmp/opentsdb-2.0.1_all.deb
 
 if [ ! -e ${OTSDB_FILE_DESTINATION} ]
 then
-    wget -o ${OTSDB_FILE_DESTINATION} ${OTSDB_URL}
+    wget -O ${OTSDB_FILE_DESTINATION} ${OTSDB_URL}
 else
     echo "${OTSDB_FILE_DESTINATION} already exists"
 fi
